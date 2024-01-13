fd25aa0821bc5ea69613c8e55ce3aa111bf5248a
sua <kimsuasch@gmail.com>
2024-01-13 17:49:04 +0900

    dkdk

--- Example/board_view.php
+++ Example/board_view.php
@@ -36,6 +36,56 @@
     $file_type = $row["file_type"];
     $file_copied = $row["file_copied"];
     $password = $row["password"];
+    
+    $content = str_replace(" ", "&nbsp;", $content);
+    $content = str_replace("\n", "<br>", $content);
+    
+    $new_hit = $hit + 1;
+    $sql = "UPDATE board_posted_data SET hit=$new_hit WHERE num=$num";
+    mysqli_query($con, $sql);
 ?>
-    </div>
+		<ul id="view_content">
+			<li>
+				<span class="col1"><b>제목 : </b><?=$subject?></span>
+				<span class="col2"><?=$name?> | <?=$regist_day?></span>
+			</li>
+			<li>
+				<?php 
+				if($file_name){
+				    $real_name = $file_copied;
+				    $file_path = "./data/".$real_name;
+				    $file_size = filesize($file_path);
+				    
+				    echo "▷ 첨부파일 : $file_name ($file_size Byte) &nbsp;&nbsp;&nbsp;&nbsp;
+                        <a href='download.php?num=$num&real_name=$real_name&file_name=$file_name&file_type=$file_type'>
+                        [ 저장 ]</a><br><br>";
+				}
+				?> 
+				<?=$content?>
+			</li>
+		</ul>
+		<ul class="buttons">
+			<li><button onclick="location.href='board_list.php?page=<?=$page?>'">목록</button></li>
+			<li><button onclick="location.href='board_modify_form.php?num=<?=$num?>&page=<?=$page?>'">수정</button></li>
+			<li><button onclick="location.href='board_delete.php?num=<?=$num?>&page=<?=$page?>'">삭제</button></li>
+			<li><button onclick="location.href='board_form.php'">글쓰기</button></li>
+		</ul>
+    </div><!-- board_box ENDS -->
 </section>
+<footer>
+	<?php include "footer.php";?>
+</footer>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
